0234106d1f2ffe4234a70a694fe35911189c475f
Kiran Kumari <kumarikiran91133963@gmail.com>
2025-08-12 13:51:59 +0000

    Fix warning

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,5 +6,5 @@ import {googleAI} from '@genkit-ai/googleai';
 // The global 'ai' object will be used for defining prompts and flows.
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-1.5-flash-latest',
+  model: 'googleai/gemini-2.5-flash-latest',
 });
