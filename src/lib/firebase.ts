046878a2be707cd881e6e30a5e308300426749c2
Kiran Kumari <kumarikiran91133963@gmail.com>
2025-08-12 16:16:25 +0000

    Use firebase SDKs           // Import the functions you need from the SD

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "skillzup-z0dj5",
-  "appId": "1:493117735774:web:1a8808c134f897130485cf",
-  "storageBucket": "skillzup-z0dj5.firebasestorage.app",
-  "apiKey": "AIzaSyCOo9HVhMExiRYZEdJy_JdXmFWBWLs10T4",
-  "authDomain": "skillzup-z0dj5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "493117735774"
+  apiKey: "AIzaSyCOo9HVhMExiRYZEdJy_JdXmFWBWLs10T4",
+  authDomain: "skillzup-z0dj5.firebaseapp.com",
+  databaseURL: "https://skillzup-z0dj5-default-rtdb.firebaseio.com",
+  projectId: "skillzup-z0dj5",
+  storageBucket: "skillzup-z0dj5.firebasestorage.app",
+  messagingSenderId: "493117735774",
+  appId: "1:493117735774:web:8ede8ed69d4790980485cf"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
